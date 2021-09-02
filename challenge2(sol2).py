12cc553dac37c4e0754bd92ebccd8440a1b3c7b6
AbdelRahman Salah11 <abdomagik80@gmial.com>
2021-09-03 00:19:01 +0200

    my simple works on python

---
+++ challenge2(sol2).py
@@ -0,0 +1,16 @@
+def favorite_cities(name):
+    if name==1:
+        return ("Cairo")
+    elif name==2:
+        return ("London")
+    elif name==3:
+        return ("Paries")
+    else:
+        return ("define the number")
+        
+        
+print('one of my favorite cities is: ',favorite_cities(1))
+
+print('one of my favorite cities is: ',favorite_cities(2))
+
+print('one of my favorite cities is: ',favorite_cities(3))
---
+++ challenge2(sol3).py
@@ -0,0 +1,7 @@
+def favorite_cities(name):
+    print("my favorite city is",name)
+    
+    
+favorite_cities("cairo")
+favorite_cities("London")
+favorite_cities("Paris")    
