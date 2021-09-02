aa2be48c2996a8ed0b7f172b59655702bbed803e
AbdelRahman Salah Makhlouf Mohamed <abdomagik80@gmail.com>
2021-09-02 14:11:04 +0200

    Add files via upload

---
+++ challange 2.py
@@ -0,0 +1,9 @@
+my_best_food=str(input("what's my favorite food"))
+
+if my_best_food!="pasta":
+    print("yep,that's amazing")
+
+else:
+    print("yek,that's not it")
+
+print("thanks for playing")    
